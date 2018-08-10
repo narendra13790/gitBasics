736da04a9784758d68691bd4fe4346a0ef51b345
Narendra <narendra13790@gmail.com>
2018-08-10 14:32:26 +0530

    adding file

---
+++ LambdaBasics.java
@@ -0,0 +1,42 @@
+package lambdaExpression;
+
+public class LambdaBasics {
+	
+	
+	public int addNum(int a , int b)
+	{
+		return a+b;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		LambdaBasics obj=new LambdaBasics();
+		
+		System.out.println(obj.addNum(10, 20));
+		
+		/// same addNum method with lambda
+		
+		//syntax 
+		//  InterfcaneName  any VaribaleName = (parameters) -> 
+		//{
+			
+		//}
+		
+		AddNum addNum=(a,b) ->{ System.out.println("Addition : "+ (a+b)) ;return (a+b);};
+		
+		
+		
+		addNum.operation(9, 6);
+		//System.out.println(addNum.operation(9, 6));
+			
+		
+		
+	}
+	@FunctionalInterface
+	interface AddNum
+	{
+		int operation(int a, int b);
+	}
+
+}
